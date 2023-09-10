6d052c5028f422eaadc306c9797657dca670c000
olamidewasiu <140614019+olamidewasiu@users.noreply.github.com>
2023-09-10 16:29:53 +0100

    Update 0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -1,1 +1,13 @@
+/*Positive or negative*/
 
+input stdio.h
+int main(void)
+{
+  if (n>0)
+    printf(n is positive);
+  else if(n<0);
+    printf(n is negative);
+  else;
+    printf(n is 0);
+  return(0)
+}
