bd9eaa49647649a327bf5ba5fc33db28c0482d1b
olamidewasiu <140614019+olamidewasiu@users.noreply.github.com>
2023-09-10 17:06:32 +0100

    Update 0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -8,12 +8,12 @@ int main(void)
 {
   int n;
   strand(time(0))
-  n = rand() - RAND_MAX /2;
+  n = rand() - RAND_MAX / 2;
   if (n>0)
     printf("%i is positive\n", n);
   else if(n<0)
     printf("%i is negative\n", n);
   else
     printf("%i is 0\n", n);
-  return(0)
+  return (0)
 }
