30a83fa073a7f2e094736589e34c15cfd72ddb69
olamidewasiu <olamidewasiu1991@gmail.com>
2023-09-18 10:36:33 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,19 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stio.h>
+#include <unistd.h>
 
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s)
+void puts2_(char, *str);
+void puts_half(char, *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src)
 
-#endi:f
+#endif
